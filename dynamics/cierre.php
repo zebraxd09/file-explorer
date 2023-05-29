0b4b78c07ae6032c90952a7d99185842cd4b3bb2
danibutnotdani <erickdardzmtz05@comunidad.unam.mx>
2023-05-29 17:58:36 -0600

    Avances dani

---
+++ dynamics/cierre.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cerrar sesión</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../Statics%20-%20Assets/style.css">
+</head>
+<body>
+<h1>Hasta luego, [nombre del usuario]</h1>
+<p>Has cerrado sesión exitosamente. ¡Esperamos verte de nuevo pronto!</p>
+<button onclick="location.href='../Templates/registro.html'">Volver al inicio de sesión</button>
+<?php
+// Aquí iría lo de cerrar la sesión del usuario.
+?>
+</body>
+</html>
---
+++ dynamics/eliminar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eliminar archivo/carpeta</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../Statics%20-%20Assets/style.css">
+</head>
+<body>
+<h1>Eliminar archivo o carpeta</h1>
+<form>
+    <label for="nombre">Nombre:</label><br>
+    <input type="text" id="nombre" name="nombre" required><br>
+
+    <input type="submit" value="Eliminar">
+</form>
+</body>
+</html>
